2b18460faeddfdf632258d8e00123113a3718f75
LuzmilaC <lcenturion@udesa.edu.ar>
2024-06-21 16:05:23 -0300

    cree js/login.js agregando funciones y verificaciones al archivo login.js

---
+++ js/login.js
@@ -0,0 +1,21 @@
+let formulario = document.querySelector("#loginForm");
+if (formulario) {
+    let email = document.querySelector("#loginEmail");
+    let contraseña = document.querySelector("#loginContraseña");
+    formulario.addEventListener("submit", function(event) {
+       event.preventDefault();
+        if(email.value === "") {
+            alert("Por favor complete el campo email");
+        } else if(contraseña.value === ""){
+            
+            alert("Por favor complete el campo contraseña");
+        } else if(contraseña.value.length < 6){
+            alert("La contraseña debe tener al menos 6 caracteres")
+
+        } else {
+            localStorage.setItem('emailUsuario', email.value);
+
+            document.location.href = "index.html"; 
+        }
+    });
+}
