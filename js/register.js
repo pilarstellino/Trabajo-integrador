21762aa7df9b74b7d77fc7f872cebe81226f8a9f
LuzmilaC <lcenturion@udesa.edu.ar>
2024-06-23 12:16:25 -0300

    cree js/register.js para cumplir con la consgna y ajuste un poco el register.html y el css/styles.css

---
+++ js/register.js
@@ -0,0 +1,35 @@
+let form = document.querySelector("#registerForm");
+    let errorDiv = document.getElementById("error");
+     errorDiv.style.color = "red"
+
+    if (form) {
+        let correo = document.querySelector("#registerEmail");
+        let clave = document.querySelector("#registerContraseña");
+        let confirmacionClave = document.querySelector("#confirmacionClave");
+
+        form.addEventListener("submit", function(event){
+            event.preventDefault(); 
+            let mensajesError = [];
+
+            if(correo.value === "") {
+                mensajesError.push("Por favor complete el campo Email.");
+            }else if(clave.value === "") {
+                mensajesError.push("Por favor complete el campo Contraseña.");
+            }else if(clave.value.length < 6) {
+                mensajesError.push("Debe ingresar al menos 6 caracteres.");
+            }else if(confirmacionClave.value!== clave.value) {
+                mensajesError.push("Las contraseñas no coinciden.");
+            }
+
+            if(mensajesError.length > 0) {
+                errorDiv.innerHTML = ""; 
+                mensajesError.forEach(function(message) {
+                    let p = document.createElement('p');
+                    p.textContent = message;
+                    errorDiv.appendChild(p);
+                });
+            } else {
+                window.location.href = "login.html";
+            }
+        });
+    }
